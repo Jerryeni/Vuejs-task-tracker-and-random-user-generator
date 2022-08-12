9a1e727f6e250fe205bbe4e6d0f07e6ca07d5060
Jeremiah Ibiang Eni <77592477+Jerryeni@users.noreply.github.com>
2022-08-12 13:49:55 +0100

    add

--- apptrack.js
+++ apptrack.js
@@ -1,10 +1,110 @@
-const app = vue.createApp({
-    template: `<h1>Task Tracker</h1>`,
+const { createApp } = Vue
+
+createApp({
     data() {
         return {
-            title: 'Task Tracker'
+            title: 'Task tracker',
+            text: '',
+            day: '',
+            reminder: false,
+            showAddTask: false,
+
+            tasks: [
+
+                {
+                    id: 1,
+                    text: 'Doctors Appointment',
+                    day: 'August 15th at 2:30pm',
+                    reminder: true,
+
+                },
+
+                {
+                    id: 2,
+                    text: 'Play football',
+                    day: 'August 15th at 2:30pm',
+                    reminder: true,
+                },
+
+                {
+                    id: 3,
+                    text: 'Wash cloths',
+                    day: 'August 15th at 2:30pm',
+                    reminder: false,
+                },
+
+            ]
+            
         }
     },
-})
 
-app.mount('#app ')
+    // created() {
+    //     this.tasks = [
+
+    //         {
+    //             id: 1,
+    //             text: 'Doctors Appointment',
+    //             day: 'August 15th at 2:30pm',
+    //             reminder: true,
+    //         },
+
+    //         {
+    //             id: 1,
+    //             text: 'Play football',
+    //             day: 'August 15th at 2:30pm',
+    //             reminder: true,
+    //         },
+
+    //         {
+    //             id: 1,
+    //             text: 'Wash cloths',
+    //             day: 'August 15th at 2:30pm',
+    //             reminder: false,
+    //         },
+
+    //     ]
+    // },   
+
+    methods: {
+        onClick(showAddTask) {
+            this.showAddTask = !showAddTask
+        },
+
+        onDelete(id) {
+            if (confirm('Are you sure?')) {
+                this.tasks = this.tasks.filter((task) => task.id !== id)
+            }
+        },
+
+        toggleReminder (id) {
+            this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task)
+        },
+
+        onSubmit(e) {
+            e.preventDefault()
+            
+            if(!this.text) {
+                alert('Please add a task')
+                return
+            }
+            const newTask = {
+                id: Math.floor(Math.random() * 100000),
+                text: this.text,
+                day: this.day,
+                reminder: this.reminder
+            }
+
+             this.tasks = [...this.tasks, newTask]
+
+             this.text = ''
+             this.day = ''
+             this.reminder = false
+        }
+
+    }
+}).mount('#app')
+
+// const app = vue.createApp({
+// })
+
+// app.mount('#app ')
